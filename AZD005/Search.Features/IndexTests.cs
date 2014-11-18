54434c698797072178670139956c4cb5fc93e52e
Alessandro Melchiori <alessandro.melchiori@codiceplastico.com>
2014-11-19 00:05:19 +0100

    added search demo

---
+++ AZD005/Search.Features/IndexTests.cs
@@ -0,0 +1,23 @@
+﻿using NUnit.Framework;
+using System;
+using System.IO;
+
+namespace Search.Features
+{
+    [TestFixture]
+    public class IndexTests
+    {
+        [Test]
+        public async void CreateIndex()
+        {
+            await SearchEnvironment.DeleteIndex("musicstoreindex");
+
+            var schema = File.ReadAllText("Support\\schema.json");
+            await SearchEnvironment.CreateIndex("musicstoreindex", schema);
+
+            await SearchEnvironment.LoadDocuments("musicstoreindex", File.ReadAllText("Support\\data1.json"));
+            await SearchEnvironment.LoadDocuments("musicstoreindex", File.ReadAllText("Support\\data2.json"));
+            await SearchEnvironment.LoadDocuments("musicstoreindex", File.ReadAllText("Support\\data3.json"));
+        }
+    }
+}
---
+++ AZD005/Search.Features/SearchEnvironment.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Search.Features
+{
+    public class SearchEnvironment
+    {
+        private static readonly String ApiVersion = "api-version=2014-07-31-Preview";
+        private static readonly String ServiceUrl = "https://codiceplastico.search.windows.net";
+        private static readonly String PrimaryKey = File.ReadAllText("keys.secret");
+
+        public static async Task<Boolean> DeleteIndex(String indexName)
+        {
+            var uri = String.Format("{0}/indexes/{1}?{2}", ServiceUrl, indexName, ApiVersion);
+
+            using (var client = new HttpClient())
+            {
+                client.DefaultRequestHeaders.Add("api-key", PrimaryKey);
+                var response = await client.DeleteAsync(uri); 
+                
+                return response.StatusCode == HttpStatusCode.NoContent || response.StatusCode == HttpStatusCode.NotFound;
+            }
+        }
+
+        public static async Task<Boolean> CreateIndex(String indexName, String schema)
+        {
+            var uri = String.Format("{0}/indexes/{1}?{2}", ServiceUrl, indexName, ApiVersion);
+
+            using (var client = new HttpClient())
+            {
+                client.DefaultRequestHeaders.Add("api-key", PrimaryKey);
+                var response = await client.PutAsync(new Uri(uri), new StringContent(schema, Encoding.UTF8, "application/json"));
+
+                return response.StatusCode == HttpStatusCode.Created;
+            }
+        }
+
+        public static async Task<Boolean> LoadDocuments(String indexName, String documents)
+        {
+            //https://codiceplastico.search.windows.net/indexes/musicstoreindex/docs/index?api-version=2014-07-31-Preview
+            var uri = String.Format("{0}/indexes/{1}/docs/index?{2}", ServiceUrl, indexName, ApiVersion);
+
+            using (var client = new HttpClient())
+            {
+                client.DefaultRequestHeaders.Add("api-key", PrimaryKey);
+                var response = await client.PostAsync(new Uri(uri), new StringContent(documents, Encoding.UTF8, "application/json"));
+
+                return response.StatusCode == HttpStatusCode.OK;
+            }
+        }
+    }
+}
